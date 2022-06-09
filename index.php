4d380493f01a66dbf328222e4d89629d82414e57
Hautin <matthiashautin>
2022-06-09 20:34:45 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Matthias Hautin</title>
+</head>
+    <body>
+        <top>
+            <div class="profil">
+                <img src="images/photo.jpg">
+            </div>
+            <p id="auteur">Matthias<br><span class="nom">Hautin</span></p>
+        </top>
+            <ul>
+                <li style="--clr:#b85700">
+                    <a href="Acceuil.php" data-text="&nbsp;Accueil">&nbsp;Accueil</a>
+                </li>
+                <li style="--clr:#ffcb9c">
+                    <a href="Profil.php" data-text="&nbsp;Profil">&nbsp;Profil</a>
+                </li>
+                <li style="--clr:#ffcb9c">
+                    <a href="BtsSio.php" data-text="&nbsp;BtsSio">&nbsp;BtsSio</a>
+                </li>
+                <li style="--clr:#ffcb9c">
+                    <a href="Techno.php" data-text="&nbsp;Veille&nbsp;Techno">&nbsp;Veille&nbsp;Techno</a>
+                </li>
+                <li style="--clr:#ffcb9c">
+                    <a href="Projet.php" data-text="&nbsp;Projet">&nbsp;Projet</a>
+                </li>
+                <li style="--clr:#ffcb9c">
+                    <a href="Contact.php" data-text="&nbsp;Contact">&nbsp;Contact</a>
+                </li>
+            </ul>
+            <div class="box">
+                <h2>1</h2>
+                <h3 class="ki">Bts Sio Slam</h3>
+            </div>
+            <div class="btn">
+                <a class="contact" class="btn" href="Profil.php">En savoir plus</a>
+            </div>                
+    </body>
+</html>
