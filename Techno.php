aef3680675e552dc48792b351c58cc6ba4d99be7
Hautin <matthiashautin>
2022-06-17 16:41:15 +0200

    17/06/22 16:41

--- Techno.php
+++ Techno.php
@@ -21,26 +21,26 @@
             </div>
             <p id="auteur">Matthias<br><span class="nom">Hautin</span></p>
             <ul>
-                <li style="--clr:#ffcb9c">
-                    <a id="lien" href="Acceuil.php" data-text="&nbsp;Accueil">&nbsp;Accueil</a>
+                <li style="--clr:#e4202d">
+                    <a id="lien" href="home.php" data-text="&nbsp;Accueil">&nbsp;Accueil</a>
                 </li>
-                <li style="--clr:#ffcb9c">
+                <li style="--clr:#e4202d">
                     <a id="lien" href="Profil.php" data-text="&nbsp;Profil">&nbsp;Profil</a>
                 </li>
-                <li style="--clr:#ffcb9c">
+                <li style="--clr:#e4202d">
                     <a id="lien" href="BtsSio.php" data-text="&nbsp;BtsSio">&nbsp;BtsSio</a>
                 </li>
-                <li style="--clr:#b85700">
+                <li style="--clr:#b4030f">
                     <a id="lien" href="Techno.php" data-text="&nbsp;Veille&nbsp;Techno">&nbsp;Veille&nbsp;Techno</a>
                 </li>
-                <li style="--clr:#ffcb9c">
+                <li style="--clr:#e4202d">
                 <a class="projeth" id="lien" href="Projet.php" data-text="&nbsp;Projet">&nbsp;Projet<span class="material-symbols-outlined">expand_more</span></a>
                         <ul1 id="projet">
                             <li1><a href="Projet.php" data-text="&nbsp;Réalisé en cours (TP)">&nbsp;Réalisé en cours (TP)</a></li1>
                             <li1><a href="Projet.php" data-text="&nbsp;Réalisé en stage">&nbsp;Réalisé en stage</a></li1>
                         </ul1>
                 </li>
-                <li style="--clr:#ffcb9c">
+                <li style="--clr:#e4202d">
                     <a id="lien" class="contact" href="Contact.php" data-text="&nbsp;Contact">&nbsp;Contact</a>
                 </li>
             </ul>
